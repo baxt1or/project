87d532b53a67c1f20ed0ba48866dd8ddb23b85c1
baxt1or <230035@centralasian.com>
2024-10-05 10:38:27 +0500

    modyfied main file

--- main.py
+++ main.py
@@ -28,3 +28,21 @@ def divide_numbers(a:float, b: float) -> float:
     if b == 0:
         raise ValueError("Denominator cannot be 0")
     return a/ b
+
+
+def multiply_numbers(a: float, b:float) -> float:
+    """Multiplication of two numbers or variables
+
+    Args:
+        a (float): the first variable
+        b (float): the second variable
+
+    Returns:
+        float: the outcome of the two numbers multiplication
+    """
+    return a*b
+
+
+
+if __name__ == "__main__":
+    multiply_numbers(1, 3)
